50431fc38ea54c0d031d4750f106b3e66e83b292
grfmbiu324568 <grfmbiu324568@gmail.com>
2021-10-30 15:52:12 +0800

    新增密碼強度效果

--- src/main/webapp/js/change_password.js
+++ src/main/webapp/js/change_password.js
@@ -1,4 +1,13 @@
 let myModal = new bootstrap.Modal(document.getElementById('myModal'))
+const indicator = document.querySelector(".indicator");
+const input = document.querySelector("#pswdnew");
+const weak = document.querySelector(".weak");
+const medium = document.querySelector(".medium");
+const strong = document.querySelector(".strong");
+const text = document.querySelector(".text");
+let regExpWeak = /[a-z]/;
+let regExpMedium = /\d+/;
+let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
 
 function updateModal(title, massage) {
     $('#modal-title').text(title);
@@ -101,3 +110,41 @@ let myModalEl = document.getElementById('myModal')
 myModalEl.addEventListener('hidden.bs.modal', function (event) {
     updateModal("", "");
 })
+
+//密碼強度
+function trigger() {
+    if (input.value != "") {
+        indicator.style.display = "block";
+        indicator.style.display = "flex";
+        if (input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong))) no = 1;
+        if (input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong)))) no = 2;
+        if (input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong)) no = 3;
+        if (no == 1) {
+            weak.classList.add("active");
+            text.style.display = "block";
+            text.textContent = "密碼強度：弱";
+            text.classList.add("weak");
+        }
+        if (no == 2) {
+            medium.classList.add("active");
+            text.textContent = "密碼強度：中等";
+            text.classList.add("medium");
+        } else {
+            medium.classList.remove("active");
+            text.classList.remove("medium");
+        }
+        if (no == 3) {
+            weak.classList.add("active");
+            medium.classList.add("active");
+            strong.classList.add("active");
+            text.textContent = "密碼強度：強";
+            text.classList.add("strong");
+        } else {
+            strong.classList.remove("active");
+            text.classList.remove("strong");
+        }
+    } else {
+        indicator.style.display = "none";
+        text.style.display = "none";
+    }
+}
