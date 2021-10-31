6280cfa3410bc27bf1dd1f3fa3fdd8fff0e5c157
headinghome330 <ccddcho@gmail.com>
2021-10-31 22:01:56 +0800

    *test

--- src/main/webapp/js/forget_password.js
+++ src/main/webapp/js/forget_password.js
@@ -5,43 +5,93 @@ $('#modal-title').text(title);
 $('#massage-content').text(massage);
 }
 
-$(function () {
-    if (localStorage.getItem("member") != null) {
-      memberData = JSON.parse(localStorage.getItem("member"));
-      if (memberData.Login === "OK") {
-        $(".dropdown-toggle:not(.btn)").text(memberData.member_name);
-        $(".dropdown-item:eq(3)").text("Log out");
-        $(".dropdown-item:eq(3)").attr("href", "./frontpage.html");
-      }
-      $(".dropdown-item:eq(3)").click(function () {
-        localStorage.removeItem("member");
-      });
-      $(".dropdown-item:eq(0),.dropdown-item:eq(1)").click(function (e) {
-        if (memberData.Login !== "OK") {
-          e.preventDefault();
-          // alert("請先登入會員")
-          updateModal("Oops!", "請先登入會員！");
-          myModal.show();
-          $(".modal-footer>button").click(function () {
-            location.href = "./login.html";
-          });
-        }
-      });
-    } else {
-      $(".dropdown-item:eq(0),.dropdown-item:eq(1)").click(function (e) {
-        e.preventDefault();
-        // alert("請先登入會員")
-        updateModal("Oops!", "請先登入會員！");
-        myModal.show();
-        $(".modal-footer>button").click(function () {
-          location.href = "./login.html";
-        });
-      });
-    }
-  });
 
-  // reset modal when modal was hidden
-  let myModalEl = document.getElementById("myModal");
-  myModalEl.addEventListener("hidden.bs.modal", function (event) {
-    updateModal("", "");
-  });
+$(function(){
+    if(localStorage.getItem('member') != null){
+        memberData = JSON.parse(localStorage.getItem('member'));
+        if(memberData.Login === 'OK'){
+        $('.dropdown-toggle:not(.btn)').text(memberData.member_name);
+        $('.dropdown-item:eq(3)').text('Log out');
+        $('.dropdown-item:eq(3)').attr('href','./frontpage.html');
+        }
+        $('.dropdown-item:eq(3)').click(function () {
+            localStorage.removeItem('member');
+        });
+        $('.dropdown-item:eq(0),.dropdown-item:eq(1)').click(function (e) {
+            if(memberData.Login !== 'OK'){
+                e.preventDefault();
+                // alert("請先登入會員")
+                updateModal("Oops!", "請先登入會員！");
+                myModal.show();
+                $('.modal-footer>button').click(function(){
+                    location.href='./login.html';
+                })
+            }
+        });
+    } else {
+        $('.dropdown-item:eq(0),.dropdown-item:eq(1)').click(function (e){
+            e.preventDefault();
+            // alert("請先登入會員")
+            updateModal("Oops!", "請先登入會員！");
+            myModal.show();
+            $('.modal-footer>button').click(function(){
+                location.href='./login.html';
+            })
+        })
+    }   
+})
+
+
+const regexEmail =  new RegExp(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
+const regexAccount = new RegExp(/^([a-zA-Z]+\d+|\d+[a-zA-Z]+)[a-zA-Z0-9]*$/ );
+$("#submit").click(function () {
+    if ($("#forgetPasswordEmail").val() === "") {
+        updateModal("Oops!", "請輸入註冊之電子信箱");
+        myModal.show();
+	} else if ($("#forgetPasswordAccount").val() === "") {
+        updateModal("Oops!", "請輸入註冊之帳號");
+        myModal.show();
+  } else if (!regexEmail.test($("#forgetPasswordEmail").val())) {
+        updateModal("Oops!", "輸入電子信箱格式不正確");
+        myModal.show();
+ 	} else if (!regexAccount.test($("#forgetPasswordAccount").val())) {
+        updateModal("Oops!", "輸入帳號格式錯誤");
+        myModal.show();
+    } else {
+		let account = $('#forgetPasswordAccount').val();
+        let email = $('#forgetPasswordEmail').val();
+        const forget_pswd = {
+            Email: email,
+			      Account:account,
+
+        }
+
+        let url = 'http://localhost:8080/Daobunso_Project/Forget_pswd';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(forget_pswd),
+            headers: { 'Content-Type': 'application/json' },
+        }).then(response => {
+            response.text()
+                .then(text => {
+                    if (text === "Success") {
+                        updateModal("發送郵件成功!", "請至信箱收取信件修改密碼。");
+                        myModal.show();
+                        $('.modal-footer>button').click(function(){
+                            location.href='./frontpage.html';
+                        })
+                    } else if (text === "Fail") {
+                        updateModal("Oops!", "必須輸入此帳號註冊時的電子信箱");
+                        myModal.show();
+                    }
+             });
+        });
+    };
+});
+
+
+
+
+
+//****************************************************** */
+
