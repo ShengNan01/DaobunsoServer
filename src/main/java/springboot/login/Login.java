98f80d1d7dce1489f042e260ac9d997101111048
kiri <jsjh355099@gmail.com>
2021-11-06 10:44:30 +0800

    app 路線  有HTTPS8443連線問題  只能用HTTP8080執行  請大家開發先暫時不要用這條喔!  等連線問題排除再使用

--- src/main/java/springboot/login/Login.java
+++ src/main/java/springboot/login/Login.java
@@ -2,12 +2,8 @@ package springboot.login;
 
 import javax.persistence.Entity;
 
+import lombok.*;
 import org.springframework.stereotype.Component;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Component
 @ToString
@@ -16,38 +12,17 @@ import lombok.ToString;
 @NoArgsConstructor
 //@Entity
 public class Login {
+	@Getter
+	@Setter
 	private String account;
+	@Getter
+	@Setter
 	private String password;
+	@Getter
+	@Setter
 	private Boolean rememberMe;
 	
 	
-	public Login() {
-		super();
-	}
-	public Login(String account, String password, Boolean rememberMe) {
-		super();
-		this.account = account;
-		this.password = password;
-		this.rememberMe = rememberMe;
-	}
-	public String getAccount() {
-		return account;
-	}
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Boolean getRememberMe() {
-		return rememberMe;
-	}
-	public void setRememberMe(Boolean rememberMe) {
-		this.rememberMe = rememberMe;
-	}
-	
+
 	
 }
