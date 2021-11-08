56f9f25b2a02827728afb0beb90a5ea8cb46b43a
ShengNan01 <grfmbiu324568@gmail.com>
2021-11-08 10:29:31 +0800

    更新

--- src/main/java/springboot/Profile/Controller/Controller.java
+++ src/main/java/springboot/Profile/Controller/Controller.java
@@ -1,22 +1,22 @@
-package springboot.Profile.Controller;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import springboot.Profile.Bean.MemberBean;
-import springboot.Profile.Service.Service;
-
-@RestController
-public class Controller {
-	@Autowired
-	Service service;
-	@PostMapping("/profile")
-	public Optional<MemberBean> getMemberProfile(@RequestBody MemberBean member) {
-	Optional<MemberBean> bean = Optional.of(service.findById(member.getMember_Id()).orElse(null));
-		return bean;
-	}
-}
+//package springboot.Profile.Controller;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import springboot.Profile.Bean.MemberBean;
+//import springboot.Profile.Service.Service;
+//
+//@RestController
+//public class Controller {
+//	@Autowired
+//	Service service;
+//	@PostMapping("/profile")
+//	public Optional<MemberBean> getMemberProfile(@RequestBody MemberBean member) {
+//	Optional<MemberBean> bean = Optional.of(service.findById(member.getMember_Id()).orElse(null));
+//		return bean;
+//	}
+//}
--- src/main/java/springboot/Profile/Service/Service.java
+++ src/main/java/springboot/Profile/Service/Service.java
@@ -1,12 +1,12 @@
-package springboot.Profile.Service;
-
-import java.util.Optional;
-
-import org.springframework.stereotype.Component;
-
-import springboot.Profile.Bean.MemberBean;
-@Component
-public interface Service {
-	
-	public Optional<MemberBean> findById(Integer memberId);
-}
+//package springboot.Profile.Service;
+//
+//import java.util.Optional;
+//
+//import org.springframework.stereotype.Component;
+//
+//import springboot.Profile.Bean.MemberBean;
+//@Component
+//public interface Service {
+//	
+//	public Optional<MemberBean> findById(Integer memberId);
+//}
