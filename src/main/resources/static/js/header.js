b0508986a3d1ddb18f23d3851cd99171eff58dcc
kiri <jsjh355099@gmail.com>
2021-11-16 18:14:04 +0800

    星星不會無限發請求了  但鳳喜的星星沒了

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,20 +1,34 @@
+// 多語
+const en = "(l='en_US')";
+const tw = "(l='zh_TW')";
+let lang = "";
+
+$('langbtn-en').click(() => {
+    lang = en;
+
+
+});
+$('langbtn-tw').click(() => {
+    lang = tw;
+});
+
+
 $(function () {
     if (getCookieByName("id") != null && getCookieByName("name") != null && getCookieByName("email") != null && getCookieByName("account") != null) {
-        $('#logintoggle').text('Log Out');
-        $('.dropdown-item:eq(3)').attr('href', './frontpage');
-        $('.dropdown-toggle:not(.btn)').text(getCookieValueByName('name'));
-        $('.dropdown-item:eq(3)').click(() => {
+        $('#login-dropdown-bottom').text('Log Out');
+        $('#login-dropdown-bottom').attr('href', './frontpage');
+        $('#nav-login').text(getCookieValueByName('name'));
+        $('#login-dropdown-bottom').click(() => {
             deleteCookie('id');
             deleteCookie('account');
             deleteCookie('name');
             deleteCookie('email');
             deleteCookie('password');
             deleteCookie('LoginOK');
-            $('.dropdown-item:eq(3)').text('Log In');
         });
     } else {
-        $('#logintoggle').text('Log In');
-        $('.dropdown-item:eq(0),.dropdown-item:eq(1)').click(function (e) {
+        $('.login-dropdown-bottom').text('Log In');
+        $('.login-dropdown-1,.login-dropdown-2').click(function (e) {
             e.preventDefault();
             updateModal("Oops!", "請先登入會員！");
             myModal.show();
@@ -22,8 +36,41 @@ $(function () {
                 location.href = "./login";
             });
         });
-        $('.dropdown-item:eq(3)').click(function () {
+        $('#login-dropdown-bottom').click(function () {
             location.assign('./login');
         });
     }
 });
+//  控制Cookie方法
+{
+    function getCookieByName(name) {
+        let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+        if (arr != null) return unescape(arr[2]);
+        return null;
+    }
+    function deleteCookie(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = getCookieByName(name);
+        if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+    function parseCookie() {
+        var cookieObj = {};
+        var cookieAry = document.cookie.split(';');
+        var cookie;
+        for (var i = 0, l = cookieAry.length; i < l; ++i) {
+            cookie = jQuery.trim(cookieAry[i]);
+            cookie = cookie.split('=');
+            cookieObj[cookie[0]] = cookie[1];
+        }
+        return cookieObj;
+    }
+    function getCookieValueByName(name) {
+        var value = parseCookie()[name];
+        if (value) {
+            value = decodeURIComponent(value);
+        }
+        return value;
+    }
+}
+//  ~控制Cookie方法
